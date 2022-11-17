db119e6391d7db2c483ba17ee03e05e6d0f3fcc4
Yordy-code81 <U201923959@upc.edu.pe>
2022-11-17 00:27:50 -0500

    feat: base project

---
+++ src/test/java/com/example/agrochillingapi/AgrochillingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agrochillingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrochillingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
